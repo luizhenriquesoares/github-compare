05a3bc65abebc620fec4db689e1735a83fb653a0
luiz henrique soares <luizhenriquesoares91@gmail.com>
2019-01-01 23:15:59 -0300

    testing styled component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-const App = () => <div className="App">Hello World </div>;
+const Title = styled.h1`
+  color: 'red';
+  font-size: 32px;
+`;
+
+const App = () => (
+  <div className="App">
+    <Title>Hello World</Title>
+  </div>
+);
 
 export default App;
